5cd55df4be2b6af2416e35b7c1b9f390765c38b5
Jusched <juans-betancurt@unilibre.edu.co>
2022-07-15 21:00:14 -0500

    Changes into match class and its methods

--- match.py
+++ match.py
@@ -1,3 +1,5 @@
+import random
+
 class Match:
 
     def __init__(self, mapa, taskList, players, sus):
@@ -7,16 +9,31 @@ class Match:
         self.sus = sus
 
 
-    def startMatch(self, mapa, players, sus):
+    def startMatch(self, players, sus, taskList):
 
         if players + sus < 10:
-            print(f"The game will start on the map {mapa} with {players} players and {sus} impostors")
+            print(f"The game will start on the map {map_choice} with {players} players and {sus} impostors")
         else:
             print(f"Select the correc amount of crewmates and impostors to start")
 
+        map_option = ("Skeld", "Mira HQ", "Polus")
+        map_choice = random.choice(map_option)
+
+        taskList = {
+            "Skeld":("Card swipe", "Reactor", "Navigation",
+            "Cables", "02 filter", "Clean Vent", "Electric"
+            ),
+            "Mira HQ":("ID Code", "Power", "Fuel", "Shields",
+            "Process data", "Diagnostics", "Scan"
+            ),
+            "Polus":("Data", "Artifacts", "Reactor", "Calibrate",
+            "Toilet", "Decontaminate", "Keys"
+            )
+
+        }
 
     def finishMatch(self, players, sus, tasksCompleted):
-        if sus == 0 or tasksCompleted == int(players * 6):
+        if sus == 0 or tasksCompleted == int(players * 4):
             print(f"Crewmates win")
         
         elif sus == players:
