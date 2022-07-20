4fbd4d530207f113c92f6e829779a524f506fdb7
Jusched <juans-betancurt@unilibre.edu.co>
2022-07-19 20:00:12 -0500

    Finished Match class, created task class to asign and added comments on all classes and methods

--- match.py
+++ match.py
@@ -1,4 +1,3 @@
-import random
 from tasks import Tasks
 
 class Match:
@@ -12,18 +11,23 @@ class Match:
 
     def startMatch(self, players, sus):  
 
-        if players + sus <= 10:
+        if players + sus <= 10 and players <= 7 and sus >= 1:
             print(f"The game will start on the map {Tasks.giveMap} with {players} players and {sus} impostors")
         else:
             print(f"Select the correct amount of crewmates and impostors to start")
+        #checks if the number of players is enough to start the match
+
 
     def finishMatch(self, players, sus, tasksCompleted):
         if sus == 0 or tasksCompleted == int(players * 4):
             print(f"Crewmates win")
-        
+    #after every death, ejection or vote, this method will be run in
+    #order to check if the requirements for the end are met
         elif sus == players:
             print(f"Impostors win")
-        
+        else:
+            return
+    
 
 
         
--- tasks.py
+++ tasks.py
@@ -16,14 +16,23 @@ class Tasks:
         }
 
     def giveMap(self, map_option):
+        #This method selects a random map then stores the 
+        #tasks assigned to that map
         map_choice = random.choice(map_option.keys)
         task_list = map_option[map_choice]
         self.giveTask(task_list)
+        #calls giveTask method using the task_list from the dictionary
 
 
     def giveTask(self, task_list):
          
-        counter = 0
+        counter = 1
         while counter != 4:
             tasksToDo = tasksToDo.append(random.choice(task_list))
             counter += 1
+            if tasksToDo[counter] in tasksToDo:
+                tasksToDo.pop(counter)
+                counter -= 1
+        #Assigns tasks from task_list until 4 are choosen. 
+        #If a task is going to be repeated, it repeats the process in order to avoid it
+    
